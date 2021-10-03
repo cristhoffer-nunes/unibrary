1ca83a7babebd34345aa9f0a26de87c14fd1265a
Cristhoffer Nunes <nunes-santos@hotmail.com>
2021-10-03 13:05:03 -0300

    feat: Adicionar rota cliente e login

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const acervo = require('./acervoRoute')
+const cliente = require('./clienteRoute')
+const login = require('./loginRoute')
 
 module.exports = app => {
   app.use(express.json())
   app.use(acervo)
+  app.use(cliente)
+  app.use(login)
 }
