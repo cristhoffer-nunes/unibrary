108e6eef2b76004ecbfae051573aefa8c52a998f
Cristhoffer Nunes <nunes-santos@hotmail.com>
2021-10-20 17:18:54 -0300

    refactor: Corrigir funções

--- controllers/LoginController.js
+++ controllers/LoginController.js
@@ -1,10 +1,24 @@
 const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
+const { InvalidArgumentError } = require('../error/erro')
+const database = require('../models')
 
 class LoginController {
-  static fazerLogin (req, res) {
-    const token = criaTokenJWT(req.user)
-    res.set('Authorization', token)
-    res.status(204).send()
+  static async login (req, res) {
+    try {
+      const numeroMatricula = req.body.numeroMatricula
+      const senha = req.body.senha
+      const usuario = await database.Clientes.findOne({ where: { numero_matricula: numeroMatricula } })
+
+      if (verificaUsuario(usuario) !== false && verificaAtivo(usuario.ativo) !== false && await verificaSenha(senha, usuario.senha) !== false) {
+        const token = criaTokenJWT(usuario)
+        return res.status(200).json({ result: token })
+      } else {
+        throw new Error()
+      }
+    } catch (error) {
+      return res.status(400).json({ error: 'Bad Request' })
+    }
   }
 }
 
@@ -13,8 +27,27 @@ function criaTokenJWT (usuario) {
     numero_matricula: usuario.numero_matricula
   }
 
-  const token = jwt.sign(payload, process.env.CHAVE_JWT)
+  const token = jwt.sign(payload, process.env.CHAVE_JWT, { expiresIn: '15m' })
   return token
+}
+
+function verificaUsuario (usuario) {
+  if (!usuario == null) {
+    return true
+  } else if (usuario == null) {
+    return false
+  }
+}
+
+async function verificaSenha (senha, senhaHash) {
+  const senhaValida = await bcrypt.compare(senha, senhaHash)
+  return senhaValida
+}
+
+function verificaAtivo (usuarioAtivo) {
+  if (usuarioAtivo === false) {
+    throw new InvalidArgumentError('Usuário invativo!')
+  }
 }
 
 module.exports = LoginController
