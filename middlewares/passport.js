eb3866f633e5e8135a49687a14dd538662d5b78f
Cristhoffer Nunes <nunes-santos@hotmail.com>
2021-10-20 17:17:55 -0300

    refactor: Remover function bear

--- middlewares/passport.js
+++ middlewares/passport.js
@@ -26,17 +26,6 @@ module.exports = {
     )(req, res, next)
   },
 
-  bear: function (req, res, next) {
-    passport.authenticate('bearer', function (err, user, info) {
-      if (err && err.name === 'JsonWebTokenError') return res.status(401).json({ err: err.message })
-      if (err && err.name === 'TokenExpiredError') return res.status(401).json({ erro: err.message, expiradoEm: err.expiredAt })
-      if (err) return res.status(500).json({ err: err.message })
-      req.token = info.token
-      req.user = user
-      return next()
-    })(req, res, next)
-  },
-
   bearer: (req, res, next) => {
     passport.authenticate(
       'bearer',
