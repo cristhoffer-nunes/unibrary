d72d3bc247e5a013d252f65402d4d1712cba4ea9
Cristhoffer Nunes <nunes-santos@hotmail.com>
2021-10-24 23:44:57 -0300

    refactor: Remover comentários

--- index.js
+++ index.js
@@ -11,9 +11,7 @@ const cors = require('cors')
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use((req, res, next) => {
-  // Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
   res.header('Access-Control-Allow-Origin', '*')
-  // Quais são os métodos que a conexão pode realizar na API
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
   app.use(cors())
   next()
