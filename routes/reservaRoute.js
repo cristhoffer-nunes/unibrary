5a12dc182432c63997b718ffd16446cbe9bf71d5
Cristhoffer Nunes <nunes-santos@hotmail.com>
2021-10-24 23:05:11 -0300

    feat: Adicionar apagaReserva

--- routes/reservaRoute.js
+++ routes/reservaRoute.js
@@ -1,9 +1,11 @@
 const { Router } = require('express')
 const ReservaController = require('../controllers/ReservaController')
 const router = Router()
+
 router
   .get('/v1/api/reserva', ReservaController.listaTodaReserva)
   .get('/v1/api/reserva/cliente', ReservaController.listaReservaCliente)
-  .post('/v1/api/reserva', ReservaController.fazUmaReserva)
+  .post('/v1/api/reserva', ReservaController.fazReserva)
+  .delete('/v1/api/reserva/:id', ReservaController.apagaReserva)
 
 module.exports = router
