7ae51a0a33843c9629167d303fe2dfaf832fcfb9
Cristhoffer Nunes <nunes-santos@hotmail.com>
2021-10-24 23:04:48 -0300

    refactor: Remover swagger description

--- routes/acervoRoute.js
+++ routes/acervoRoute.js
@@ -1,18 +1,8 @@
 const { Router } = require('express')
 const AcervoController = require('../controllers/AcervoController')
-const passport = require('passport')
 const router = Router()
 
-router.get(
-  /*
-    #swagger.description = 'Rota para listar todas as obras do acervo.'
-    */
-
-  '/v1/api/acervo', AcervoController.listaTodoAcervo)
-router.get(
-  /*
-    #swagger.description = 'Rota para listar uma obra por id.'
-    */
-  '/v1/api/acervo/:id', passport.authenticate('bearer', { session: false }), AcervoController.pegaUmaObra)
+router.get('/v1/api/acervo', AcervoController.listaTodoAcervo)
+router.get('/v1/api/acervo/:id', AcervoController.pegaUmaObra)
 
 module.exports = router
