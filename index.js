6e8c8c19a0a1f02cd726cf7ee42e30ee5f437311
Cristhoffer Nunes <nunes-santos@hotmail.com>
2021-10-24 23:44:29 -0300

    refactor: Cors

--- index.js
+++ index.js
@@ -10,7 +10,14 @@ const cors = require('cors')
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
-app.use(cors())
+app.use((req, res, next) => {
+  // Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
+  res.header('Access-Control-Allow-Origin', '*')
+  // Quais são os métodos que a conexão pode realizar na API
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+  app.use(cors())
+  next()
+})
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 
 routes(app)
