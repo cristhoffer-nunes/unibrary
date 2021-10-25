fe414ca8185820376cb7ed04fa0fd46dddf2715b
Cristhoffer Nunes <nunes-santos@hotmail.com>
2021-10-24 23:03:46 -0300

    refactor: Adicionar metodo delete e corrigir fazReserva

--- controllers/ReservaController.js
+++ controllers/ReservaController.js
@@ -4,6 +4,7 @@ class ReservaController {
   static async listaTodaReserva (req, res) {
     try {
       const todaReserva = await database.Reserva.findAll()
+      console.log(todaReserva)
       return res.status(200).json(todaReserva)
     } catch (error) {
       return res.status(400).json({ error: error.message })
@@ -11,8 +12,8 @@ class ReservaController {
   }
 
   static async listaReservaCliente (req, res) {
+    const numeroMatricula = req.body.numeroMatricula
     try {
-      const numeroMatricula = req.body.numeroMatricula
       const pegaCliente = await database.Clientes.findOne({ where: { numero_matricula: numeroMatricula } })
       const pegaReservaCliente = await database.Reserva.findAll({ where: { cliente_id: Number(pegaCliente.id) } })
       return res.status(200).json(pegaReservaCliente)
@@ -21,21 +22,37 @@ class ReservaController {
     }
   }
 
-  static async fazUmaReserva (req, res) {
+  static async fazReserva (req, res) {
     const numeroMatricula = req.body.numeroMatricula
     const obraId = req.body.obra
     try {
       const pegaCliente = await database.Clientes.findOne({ where: { numero_matricula: numeroMatricula } })
       const pegaObra = await database.Acervo.findOne({ where: { id: Number(obraId) } })
-      const novaReserva = await database.Reserva.create({
-        data_reserva: new Date(),
-        acervo_id: pegaObra.id,
-        cliente_id: pegaCliente.id
-      })
+      const { count } = await database.Reserva.findAndCountAll({ where: { cliente_id: pegaCliente.id } })
 
-      return res.status(201).json(novaReserva)
+      if (count >= 2) {
+        throw new Error('Número máximo de reservas atingido.')
+      } else {
+        const novaReserva = await database.Reserva.create({
+          data_reserva: new Date(),
+          acervo_id: pegaObra.id,
+          cliente_id: pegaCliente.id
+        })
+        await database.Acervo.update({ qtd_obras: pegaObra.qtd_obras - 1 }, { where: { id: pegaObra.id } })
+        return res.status(201).json(novaReserva)
+      }
     } catch (error) {
+      return res.status(400).json({ error: 'Número máximo de reservas atingido.' })
+    }
+  }
 
+  static async apagaReserva (req, res) {
+    const { id } = req.params
+    try {
+      const apagaReserva = await database.Reserva.destroy({ where: { id: Number(id) } })
+      return res.status(200).json(apagaReserva)
+    } catch (error) {
+      return res.status(400).json(error.message)
     }
   }
 }
