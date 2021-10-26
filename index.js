af52a07357b886ca03209f9ecfdaadefab5031b0
Cristhoffer Nunes <nunes-santos@hotmail.com>
2021-10-26 11:07:41 -0300

    refactor: Corrigir cors

--- index.js
+++ index.js
@@ -6,16 +6,15 @@ const port = process.env.PORT || 8080
 const routes = require('./routes')
 const swaggerUi = require('swagger-ui-express')
 const swaggerFile = require('./swagger/swagger_output.json')
-const cors = require('cors')
+const cors = require('express-cors')
 
 app.use(express.json())
+app.use(cors({
+  allowedOrigins: [
+    'localhost:3000', 'http://localhost:3000', 'https://localhost:3000'
+  ]
+}))
 app.use(express.urlencoded({ extended: false }))
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*')
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
-  app.use(cors())
-  next()
-})
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 
 routes(app)
